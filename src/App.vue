f80c118319fff9b2fad440c39b24fe21183957d1
sanjuanraven15 <163710791+sanjuanraven15@users.noreply.github.com>
2024-04-10 20:03:14 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <router-view></router-view>
+    <template v-if="!isAuthenticated">
+      <LoginForm />
+    </template>
+    <template v-else>
+      <div>
+        <h2>Welcome, {{ user.name }}</h2>
+        <p>Status: {{ user.status }}</p>
+        <template v-if="user.status !== 'for_verification'">
+          <BookList />
+          <SearchBar />
+          <ShoppingCart v-if="user.status === 'verified'" />
+        </template>
+        <p v-else>Please wait for verification to access the shopping cart.</p>
+        <AdminPanel v-if="user.username === 'admin'" />
+        <button @click="logout">Log Out</button> <!-- Log out button for all users -->
+      </div>
+    </template>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { mapState } from 'vuex'
+import LoginForm from './components/LoginForm.vue'
+import BookList from './components/BookList.vue'
+import SearchBar from './components/SearchBar.vue'
+import ShoppingCart from './components/ShoppingCart.vue'
+import AdminPanel from './components/AdminPanel.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LoginForm,
+    BookList,
+    SearchBar,
+    ShoppingCart,
+    AdminPanel
+  },
+  computed: {
+    ...mapState(['isAuthenticated', 'user'])
+  },
+  methods: {
+    logout() {
+      this.$store.commit('updateAuthenticationStatus', false);
+      this.$store.commit('setUser', null);
+      this.$router.push('/login');
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
+
